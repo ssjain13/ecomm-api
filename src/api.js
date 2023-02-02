9d6888644e52f9ac472849554ae0ccf46e6a57c2
Chaplot <sonal.chaplot@accenture.com>
2023-02-02 10:52:19 +0530

    cors header changes

--- src/api.js
+++ src/api.js
@@ -24,8 +24,8 @@ app.all('*', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 }); */
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://ssjain13.github.io/"); // Update to match the domain you will make the request from
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // Update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
